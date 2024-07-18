dbcdee38428e5f2f6fc614481a1e2d8f8fa64136
Linsarthur <linsarthur02@gmail.com>
2024-07-18 13:37:42 -0300

    feito a pagina not found

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import '../styles/Navbar.css'
 const Navbar = () => {
   return (
     <div>
-      Navbar
+      
     </div>
   )
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,19 @@
 import '../styles/NotFound.css'
-
+import error from "../assets/error404.webp"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <>
+    <Navbar />
+    <main>
+    <section className='container'>
+      <img className='img404' src={error} alt="Página não localizada" />
+      <h2>Ops! Conteúdo não localizado! 🧐 </h2>
+    </section>
+    </main>
+    <Footer />
+    </>
   )
 }
 
