def3b0c647cc8d3287bf1cc7e1970e8c5f7a6e36
Linsarthur <linsarthur02@gmail.com>
2024-07-18 15:30:54 -0300

    Feito a página sobre

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import '../styles/Home.css'
-import { Button} from "react-bootstrap";
+
 const Home = () => {
   return (
     <>
-    <main>
+    <main className='home'>
       <div>
-      <h1 className='aga1'>Bem vindo!</h1>
-      <p>Este é o local para amantes de livros. Sua biblioteca pessoal!</p>
+        <h1 className='aga1'>Bem vindo(a)!</h1>
+        <p>Este é o local para amantes de livros. Sua biblioteca pessoal!</p>
       </div>
     </main>
     
