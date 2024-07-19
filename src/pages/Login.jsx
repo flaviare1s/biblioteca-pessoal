85adf9c98fe67f2a69d3bb101f58b0dad0aa07d7
Moises Almeida <moisesssprg@gmail.com>
2024-07-19 12:15:19 -0300

    fix: corrige funcionalidades de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -33,20 +33,6 @@ const Login = () => {
       });
   }
 
-  function deletarLivro(id) {
-    deleteLivro(id).then(() => {
-      toast.success('Livro removido com sucesso!');
-      carregarLivros();
-    }).catch((error) => {
-      console.error('Erro ao deletar livro:', error);
-      toast.error('Erro ao deletar livro');
-    });
-  }
-
-  useEffect(() => {
-    carregarLivros();
-  }, []);
-
   return (
     <main>
       <Form className='form-section' onSubmit={handleSubmit(onSubmit)}>
@@ -81,58 +67,12 @@ const Login = () => {
             Entrar com Google 
           </Button>
         </div>
-        {livros ?
-          <Row xs={1} sm={2} md={3} lg={4} className='g-4 p-3 justify-content-center align-items-center'>
-            {livros.map((livro) => (
-              <Col key={livro.id}>
-                <Card className='h-100 card-custom'>
-                  <Card.Body>
-                    <Card.Title>{livro.titulo}</Card.Title>
-                    <Card.Text>Autor: {livro.autor}</Card.Text>
-                    <Card.Text>Editora: {livro.editora}</Card.Text>
-                    <Card.Text>{livro.descricao}</Card.Text>
-                        
-                    <div className='avaliacao mt-2'>
-                      {[1, 2, 3, 4, 5].map((valor) => (
-                        <img
-                          key={valor}
-                          src={valor <= livro.avaliacao ? filledStar : star}
-                          alt={`Estrela ${valor}`}
-                          className='star'
-                        />
-                      ))}
-                    </div>
-                  
-                    <div className='mb-2 mt-4'>
-                      <Badge bg='danger' className='me-2'>{livro.categorias}</Badge>
-                      {livro.lido ? <Badge bg='success'>Lido</Badge> : <Badge bg='warning'>Não Lido</Badge>}
-                    </div>
-
-                    <div className='btn-group mt-3' role='group'>
-                      <Button variant='outline-dark' className="d-flex align-items-center justify-content-center"
-                        onClick={() => navigate(`/livros/editar/${livro.id}`)}>
-                        <span className="material-symbols-outlined">edit_note</span>
-                      </Button>
-                      <Button variant='outline-secondary' className="d-flex align-items-center justify-content-center"
-                        onClick={() => navigate(`/livros/${livro.id}`)}>
-                      <span className="material-symbols-outlined">visibility</span>
-                      </Button>
-                      <Button variant='outline-danger' className="d-flex align-items-center justify-content-center"
-                        onClick={() => deletarLivro(livro.id)}>
-                        <span className="material-symbols-outlined">delete</span>
-                      </Button>
-                    </div>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-          :
-          <Loader />
-        }
-      </section>
+        <div className="mt-3">
+          <small>Não possui conta? <Link to="/cadastro">Criar conta</Link></small>
+        </div>
+      </Form>
     </main>
-  );
+  )
 }
 
-export default Livros;
+export default Login
