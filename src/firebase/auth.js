0f491dd2438b0753bb81cca3c02127c8028f2031
Moises Almeida <moisesssprg@gmail.com>
2024-07-19 13:14:10 -0300

    feat: configura função de logout

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup, updateProfile } from "firebase/auth";
 import { auth } from "./config";
 
 export async function cadastrarUsuario(nome, email, senha) {
@@ -14,4 +14,8 @@ export async function entrarGoogle() {
 
 export async function loginUsuario(email, senha) {
   await signInWithEmailAndPassword(auth, email, senha);
+}
+
+export async function logout() {
+  await signOut(auth);
 }
