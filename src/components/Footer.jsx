11193ec9e9e82edf9ff610038bdea03cab7d5d82
Linsarthur <linsarthur02@gmail.com>
2024-07-19 10:24:32 -0300

    Merge do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
 import '../styles/Footer.css'
+import {Link} from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <>
+            <div className="div-footer">
+                <h2>©️2024 Todos os direitos reservados. Turma React + Node.</h2>
+                <Link className="link" to="/politica">Politicas de Privacidade</Link>
+            </div>
+        </>
+    )
 }
 
 export default Footer
