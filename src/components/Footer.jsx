6aba199edb01237a2d1cd499bcb2788f3518cb05
flaviare1s <flaviarster@gmail.com>
2024-07-22 19:17:10 -0300

    Adicionados nomes e links ao Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,12 @@ const Footer = () => {
     return (
         <>
             <footer className="div-footer mt-3">
-                <h2>©️2024 Todos os direitos reservados. Turma React + Node.</h2>
-                <Link className="link" to="/politica">Política de Privacidade</Link>
+                <p className='text-center'>©️2024 - Desenvolvido por 
+                    <br />
+                    <a href='https://github.com/flaviare1s' target='_blank' className='fw-bold text-decoration-none text-dark'> Flávia Reis</a>, 
+                    <a href='https://github.com/Linsarthur' target='_blank' className='fw-bold text-decoration-none text-dark'> Arthur Cavalcante</a> e 
+                    <a href='https://github.com/MoisesssDev' target='_blank' className='fw-bold text-decoration-none text-dark'> Moisés Almeida</a>.</p>
+                <Link className="link" to="/politica">Veja nossa Política de Privacidade</Link>
             </footer>
         </>
     )
